f77b8f7cc035877eabe1bdf7f8852bf30f138ac7
daler3 <daniroma3@gmail.com>
2016-10-02 15:30:40 +0300

    Basic files uploaded

---
+++ assignment1/sha3.h
@@ -0,0 +1,14 @@
+/* Implement the following API. Do NOT modify the given prototypes. */
+
+/* Compute the SHA-3 hash for a message.
+ *
+ * d - the output buffer (allocated by the caller)
+ * s - size of the output buffer in bits
+ * m - the input message
+ * l - size of the input message in bits
+ */
+void sha3(unsigned char *d, unsigned int s, const unsigned char *m,
+	  unsigned int l);
+
+/* You can add your own functions below this line.
+ * Do NOT modify anything above. */
